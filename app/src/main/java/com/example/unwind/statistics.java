e717d7471827f779621ce294896a744ec5f2f2df
Reeve Jarvis <rjarvis@northislandcollege.ca>
2022-06-02 19:37:41 -0700

    Final updates to placeholders and layouts

--- app/src/main/java/com/example/unwind/statistics.java
+++ app/src/main/java/com/example/unwind/statistics.java
@@ -8,11 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link statistics#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class statistics extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -28,14 +23,6 @@ public class statistics extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment statistics.
-     */
     // TODO: Rename and change types and number of parameters
     public static statistics newInstance(String param1, String param2) {
         statistics fragment = new statistics();
